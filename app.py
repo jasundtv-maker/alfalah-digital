946858983ec486c96436e6bf49534a230b624230
jasundtv-maker <jasundtv@gmail.com>
2026-06-16 00:49:40 +0700

    Update app.py

--- app.py
+++ app.py
@@ -104,10 +104,10 @@ menu = st.sidebar.radio(
 
 if menu == "🏠 Dashboard":
 
-    st.image(
-        "banner_masjid.png",
-        use_container_width=True
-    )
+   st.image(
+       "banner_masjid.jpg.png",
+       use_container_width=True
+   )
 
     st.title("🕌 APP MASJID JAMI AL-FALAH")
 
